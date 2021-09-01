ddda716f3a0927c086b2430313fa28d09f116907
Sujay Chand <sujaygchand@gmail.com>
2021-09-01 16:36:25 +1200

    Gym Create and Update Dto

---
+++ NZMartialArtsGymFinderAPI/Models/DTOs/GymCreateDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using static NZMartialArtsGymFinderAPI.Models.Gym;
+
+namespace NZMartialArtsGymFinderAPI.Models.DTOs
+{
+	public class GymCreateDto
+	{
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		public ICollection<MartialArts> MartialArtsTaught { get; set; }
+
+		public string Website { get; set; }
+
+		public byte[] Picture { get; set; }
+
+		[Required]
+		public string Address { get; set; }
+		[Required]
+		public string ZipCode { get; set; }
+		public string ContactEmail { get; set; }
+		[Required]
+		public int RegionId { get; set; }
+	}
+}
