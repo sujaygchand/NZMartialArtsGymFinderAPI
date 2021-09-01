36cd9e4df1f232e4befe0c6fe0f625bc10ac01f9
Sujay Chand <sujaygchand@gmail.com>
2021-09-01 14:48:26 +1200

    Added user models

---
+++ NZMartialArtsGymFinderAPI/Models/AuthenticationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NZMartialArtsGymFinderAPI.Models
+{
+	public class AuthenticationModel
+	{
+		[Required]
+		public string Username { get; set; }
+		[Required]
+		public string Password { get; set; }
+		public string Role { get; set; }
+	}
+}
