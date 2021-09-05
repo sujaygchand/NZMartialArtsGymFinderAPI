01c96d0c5bbd19a2e891884fd57298f68f97f5d5
Sujay Chand <sujaygchand@gmail.com>
2021-09-06 01:55:07 +1200

    Forced base model input to start order

--- NZMartialArtsGymFinderAPI/Models/BaseModel.cs
+++ NZMartialArtsGymFinderAPI/Models/BaseModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,11 @@ namespace NZMartialArtsGymFinderAPI.Models
 	public class BaseModel
 	{
 		[Key]
+		[Column(Order = 0)]
 		public int Id { get; set; }
 
 		[Required]
+		[Column(Order = 1)]
 		public string Name { get; set; }
 	}
 }
