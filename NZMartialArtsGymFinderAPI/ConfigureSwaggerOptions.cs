27be1948204f6555b418a389c261c19f0def84c7
Sujay Chand <sujaygchand@gmail.com>
2021-09-07 01:53:12 +1200

    Added Bearer token authorization to the Swashbuckle page

--- NZMartialArtsGymFinderAPI/ConfigureSwaggerOptions.cs
+++ NZMartialArtsGymFinderAPI/ConfigureSwaggerOptions.cs
@@ -13,17 +13,62 @@ namespace NZMartialArtsGymFinderAPI
 {
 	public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
 	{
+		private SwaggerGenOptions swaggerGenOptions;
 		public void Configure(SwaggerGenOptions options)
 		{
-			options.SwaggerDoc("NZMAGFOpenAPISpec", new OpenApiInfo()
+			swaggerGenOptions = options ?? throw new Exception("SwaggerGenOptions is null");
+
+			swaggerGenOptions.SwaggerDoc("NZMAGFOpenAPISpec", new OpenApiInfo()
 			{
 				Title = "NZ Martial Arts Gym Finder API",
 				Version = "1"
 			});
 
+
+			InitialiseXmlDocumentation();
+			InitialiseBearerSecurityRequirement();
+		}
+
+		private void InitialiseBearerSecurityRequirement()
+		{
+			const string Bearer = "Bearer";
+
+			swaggerGenOptions.AddSecurityDefinition(Bearer, new OpenApiSecurityScheme
+			{
+				Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
+			   "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+			   "Example: \"Bearer 12345abcdef\"",
+				Name = "Authorization",
+				In = ParameterLocation.Header,
+				Type = SecuritySchemeType.ApiKey,
+				Scheme = Bearer
+			});
+
+			swaggerGenOptions.AddSecurityRequirement(new OpenApiSecurityRequirement()
+			{
+				{
+					new OpenApiSecurityScheme
+					{
+						Reference = new OpenApiReference
+						{
+							Type = ReferenceType.SecurityScheme,
+							Id = Bearer
+						},
+						Scheme = "oauth2",
+						Name = Bearer,
+						In = ParameterLocation.Header
+					},
+					new List<string>()
+				}
+			});
+
+		}
+
+		private void InitialiseXmlDocumentation()
+		{
 			var xmlCommentFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
 			var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFileName);
-			options.IncludeXmlComments(xmlCommentsFullPath);
+			swaggerGenOptions.IncludeXmlComments(xmlCommentsFullPath);
 		}
 	}
 }
