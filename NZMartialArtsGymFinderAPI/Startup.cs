e50b50a6ef899bf1d5315dc0e651b93033731466
Sujay Chand <sujaygchand@gmail.com>
2021-09-06 23:46:12 +1200

    Added JWT bearer authentication to startup.cs

--- NZMartialArtsGymFinderAPI/Startup.cs
+++ NZMartialArtsGymFinderAPI/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,14 +8,17 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using NZMartialArtsGymFinderAPI.Data;
 using NZMartialArtsGymFinderAPI.Mappers;
 using NZMartialArtsGymFinderAPI.Repositories;
 using NZMartialArtsGymFinderAPI.Repositories.IRepositories;
+using ParkyAPI;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace NZMartialArtsGymFinderAPI
@@ -28,20 +32,47 @@ namespace NZMartialArtsGymFinderAPI
 
 		public IConfiguration Configuration { get; }
 
+		private const string APPLICATION_SETTINGS_SECTION = "ApplicationSettings";
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddCors();
 			services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-			services.AddScoped<IRegionRepository , RegionRepository>();
+			services.AddScoped<IRegionRepository, RegionRepository>();
 			services.AddScoped<IMartialArtsRepository, MartialArtsRepository>();
 			services.AddScoped<IGymRepository, GymRepository>();
+			services.AddScoped<IUserRepository, UserRepository>();
 
 			services.AddAutoMapper(typeof(MartialArtsGymFinderMappings));
 
 			services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 			services.AddSwaggerGen();
+
+			var appSettingsSection = Configuration.GetSection(APPLICATION_SETTINGS_SECTION);
+			services.Configure<ApplicationSettings>(appSettingsSection);
+			var appSettings = appSettingsSection.Get<ApplicationSettings>();
+			services.AddScoped<ApplicationSettings>(k => appSettings);
+
+			var appKey = Encoding.ASCII.GetBytes(appSettings.Secret);
+			services.AddAuthentication(k =>
+			{
+				k.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				k.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			}).AddJwtBearer(k =>
+			{
+				k.RequireHttpsMetadata = false;
+				k.SaveToken = true;
+				k.TokenValidationParameters = new TokenValidationParameters
+				{
+					IssuerSigningKey = new SymmetricSecurityKey(appKey),
+					ValidateIssuerSigningKey = true,
+					ValidateIssuer = false,
+					ValidateAudience = true
+				};
+			});
+
 			services.AddControllers();
 		}
 
@@ -55,12 +86,20 @@ namespace NZMartialArtsGymFinderAPI
 
 			app.UseHttpsRedirection();
 			app.UseSwagger();
-			app.UseSwaggerUI(options => {
+
+			app.UseSwaggerUI(options =>
+			{
 				options.SwaggerEndpoint("/swagger/NZMAGFOpenAPISpec/swagger.json", "NZ Martial Arts Gym Finder API");
 				options.RoutePrefix = "";
 			});
+
 			app.UseRouting();
 
+			// Allows methods from different API versions to be used
+			app.UseCors(k => k.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+			// Always Authenticate before Authorize
+			app.UseAuthentication();
 			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
