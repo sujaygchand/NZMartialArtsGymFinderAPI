38377f6a2798d06a7937063250336d40f82718f2
Sujay Chand <sujaygchand@gmail.com>
2021-09-07 00:11:55 +1200

    Started usercontroller add authentication method

---
+++ NZMartialArtsGymFinderAPI/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZMartialArtsGymFinderAPI.Models;
+using NZMartialArtsGymFinderAPI.Repositories.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NZMartialArtsGymFinderAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private readonly IUserRepository _userRepo;
+
+		public UserController(IUserRepository userRepo)
+		{
+			_userRepo = userRepo ?? throw new Exception("UserRepository is null");
+		}
+
+		[AllowAnonymous]
+		[HttpPost("authenticate")]
+		public IActionResult Authenticate([FromBody] AuthenticationModel model)
+		{
+			var user = _userRepo.Authenticate(model.Username, model.Password);
+
+			if (user == null)
+				 return BadRequest(new { message = "Username or password is incorrect" });
+
+			return Ok();
+		}
+	}
+}
