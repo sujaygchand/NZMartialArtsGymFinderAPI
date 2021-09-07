c61cca830ee7a56b9a1e89ec8ab0e2f225df58f7
Sujay Chand <sujaygchand@gmail.com>
2021-09-07 21:18:33 +1200

    Removed password from userdto

--- NZMartialArtsGymFinderAPI/Models/DTOs/UserDto.cs
+++ NZMartialArtsGymFinderAPI/Models/DTOs/UserDto.cs
@@ -9,7 +9,6 @@ namespace NZMartialArtsGymFinderAPI.Models.DTOs
 	{
 		public int Id { get; set; }
 		public string Username { get; set; }
-		public string Password { get; set; }
 		public string Role { get; set; }
 		public string Token { get; set; }
 	}
